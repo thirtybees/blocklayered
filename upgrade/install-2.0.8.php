a6faa48073ea214ea7c449994efe05c1e8204805
Michael Dekker <git@michaeldekker.nl>
2017-06-29 07:40:36 +0200

    Cleanup part 1

--- upgrade/install-2.0.8.php
+++ upgrade/install-2.0.8.php
@@ -1,25 +1,49 @@
 <?php
+/**
+ * 2007-2016 PrestaShop
+ *
+ * Thirty Bees is an extension to the PrestaShop e-commerce software developed by PrestaShop SA
+ * Copyright (C) 2017 Thirty Bees
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@thirtybees.com so we can send you a copy immediately.
+ *
+ * @author    Thirty Bees <modules@thirtybees.com>
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2017 Thirty Bees
+ * @copyright 2007-2016 PrestaShop SA
+ * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  PrestaShop is an internationally registered trademark & property of PrestaShop SA
+ */
 
-if (!defined('_PS_VERSION_'))
-	exit;
+if (!defined('_TB_VERSION_')) {
+    exit;
+}
 
 function upgrade_module_2_0_8($object)
 {
-	$return = Configuration::updateValue('PS_LAYERED_FILTER_PRICE_ROUNDING', 1);
-	$query = 'ALTER TABLE `'._DB_PREFIX_.'layered_indexable_attribute_group_lang_value` CHANGE `url_name` `url_name` VARCHAR( 128 ) NULL DEFAULT NULL , CHANGE `meta_title` `meta_title` VARCHAR( 128 ) NULL DEFAULT NULL';
-	$return &= Db::getInstance()->execute($query);
+    $return = Configuration::updateValue('PS_LAYERED_FILTER_PRICE_ROUNDING', 1);
+    $query = 'ALTER TABLE `'._DB_PREFIX_.'layered_indexable_attribute_group_lang_value` CHANGE `url_name` `url_name` VARCHAR( 128 ) NULL DEFAULT NULL , CHANGE `meta_title` `meta_title` VARCHAR( 128 ) NULL DEFAULT NULL';
+    $return &= Db::getInstance()->execute($query);
 
-	$query = 'ALTER TABLE `'._DB_PREFIX_.'layered_indexable_attribute_lang_value` CHANGE `url_name` `url_name` VARCHAR( 128 ) NULL DEFAULT NULL , CHANGE `meta_title` `meta_title` VARCHAR( 128 ) NULL DEFAULT NULL';
-	$return &= Db::getInstance()->execute($query);
+    $query = 'ALTER TABLE `'._DB_PREFIX_.'layered_indexable_attribute_lang_value` CHANGE `url_name` `url_name` VARCHAR( 128 ) NULL DEFAULT NULL , CHANGE `meta_title` `meta_title` VARCHAR( 128 ) NULL DEFAULT NULL';
+    $return &= Db::getInstance()->execute($query);
 
-	$query = 'ALTER TABLE `'._DB_PREFIX_.'layered_indexable_feature_lang_value` CHANGE `url_name` `url_name` VARCHAR( 128 ) NULL DEFAULT NULL , CHANGE `meta_title` `meta_title` VARCHAR( 128 ) NULL DEFAULT NULL';
-	$return &= Db::getInstance()->execute($query);
+    $query = 'ALTER TABLE `'._DB_PREFIX_.'layered_indexable_feature_lang_value` CHANGE `url_name` `url_name` VARCHAR( 128 ) NULL DEFAULT NULL , CHANGE `meta_title` `meta_title` VARCHAR( 128 ) NULL DEFAULT NULL';
+    $return &= Db::getInstance()->execute($query);
 
-	$query = 'ALTER TABLE `'._DB_PREFIX_.'layered_indexable_feature_value_lang_value` CHANGE `url_name` `url_name` VARCHAR( 128 ) NULL DEFAULT NULL , CHANGE `meta_title` `meta_title` VARCHAR( 128 ) NULL DEFAULT NULL';
-	$return &= Db::getInstance()->execute($query);
+    $query = 'ALTER TABLE `'._DB_PREFIX_.'layered_indexable_feature_value_lang_value` CHANGE `url_name` `url_name` VARCHAR( 128 ) NULL DEFAULT NULL , CHANGE `meta_title` `meta_title` VARCHAR( 128 ) NULL DEFAULT NULL';
+    $return &= Db::getInstance()->execute($query);
 
-	$query = 'ALTER TABLE `'._DB_PREFIX_.'layered_product_attribute` ADD PRIMARY KEY (`id_attribute`, `id_product`), DROP KEY `id_attribute`, ADD UNIQUE KEY `id_attribute_group` (`id_attribute_group`,`id_attribute`,`id_product`)';
-	$return &= Db::getInstance()->execute($query);
+    $query = 'ALTER TABLE `'._DB_PREFIX_.'layered_product_attribute` ADD PRIMARY KEY (`id_attribute`, `id_product`), DROP KEY `id_attribute`, ADD UNIQUE KEY `id_attribute_group` (`id_attribute_group`,`id_attribute`,`id_product`)';
+    $return &= Db::getInstance()->execute($query);
 
-	return $return;
+    return $return;
 }
