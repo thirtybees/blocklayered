bd1249132d1fd5c33c27886034280a6794d01e90
Markus Hitter <mah@jump-ing.de>
2018-07-18 22:21:09 +0200

    Make release 3.0.3.

--- blocklayered.php
+++ blocklayered.php
@@ -43,7 +43,7 @@ class BlockLayered extends Module
     {
         $this->name = 'blocklayered';
         $this->tab = 'front_office_features';
-        $this->version = '3.0.2';
+        $this->version = '3.0.3';
         $this->author = 'thirty bees';
         $this->need_instance = 0;
         $this->bootstrap = true;
