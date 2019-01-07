71b306a0830aceed06ecf994cc43f6e1902469a3
Markus Hitter <mah@jump-ing.de>
2019-01-07 17:32:08 +0100

    Remove feature_form.tpl retrocompatibility version.

--- blocklayered.php
+++ blocklayered.php
@@ -1631,11 +1631,7 @@ class BlockLayered extends Module
             ]
         );
 
-        if (version_compare(_PS_VERSION_, '1.6.0', '>=') === true) {
-            return $this->display(__FILE__, 'feature_form_1.6.tpl');
-        } else {
-            return $this->display(__FILE__, 'feature_form.tpl');
-        }
+        return $this->display(__FILE__, 'feature_form.tpl');
     }
 
     public function hookAfterSaveFeatureValue($params)
