dae936a0963b354551f924998ec2fdc77858900e
Petr Hučík <petr@getdatakick.com>
2024-09-27 10:03:42 +0200

    Release blocklayered v3.2.1

--- blocklayered.php
+++ blocklayered.php
@@ -57,7 +57,7 @@ class BlockLayered extends Module
     {
         $this->name = 'blocklayered';
         $this->tab = 'front_office_features';
-        $this->version = '3.2.0';
+        $this->version = '3.2.1';
         $this->author = 'thirty bees';
         $this->need_instance = 0;
         $this->bootstrap = true;
